321ee57b92dc2384c86133da56f2fbcc5af80bf2
Mason Roberts <mrobe18@gmail.com>
2025-04-30 16:48:57 -0400

    card and list view working

--- app/controllers/zuke_controller.rb
+++ app/controllers/zuke_controller.rb
@@ -2,6 +2,8 @@ class ZukeController < ApplicationController
   def index; end
 
   def music
+    @view_mode = params[:view] || session[:song_view_mode] || "card"
+    session[:song_view_mode] = @view_mode
     @songs = Song.all
   end
 end
