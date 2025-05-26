2954a76caf02a662bf79b455aed7d78a886d8297
Mason Roberts <mrobe18@gmail.com>
2025-05-25 22:24:30 -0400

    allow id null values, temp

--- db/migrate/20250525033022_add_artist_and_album_to_songs.rb
+++ db/migrate/20250525033022_add_artist_and_album_to_songs.rb
@@ -10,8 +10,8 @@ class AddArtistAndAlbumToSongs < ActiveRecord::Migration[8.0]
       t.string :title
       t.string :song_image_url
       t.string :song_file_url
-      t.references :artist, null: false, foreign_key: true
-      t.references :album, null: false, foreign_key: true
+      t.references :artist, null: true, foreign_key: true
+      t.references :album, null: true, foreign_key: true
       # Add other columns your app needs (duration, track_number, etc.)
       t.timestamps
     end
