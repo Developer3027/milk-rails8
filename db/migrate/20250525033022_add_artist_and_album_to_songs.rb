b227d3264d15b63a37c985697504d094bfb8ca1d
Mason Roberts <mrobe18@gmail.com>
2025-05-25 23:07:51 -0400

    set ids migration to true

--- db/migrate/20250525033022_add_artist_and_album_to_songs.rb
+++ db/migrate/20250525033022_add_artist_and_album_to_songs.rb
@@ -1,6 +1,6 @@
 class AddArtistAndAlbumToSongs < ActiveRecord::Migration[8.0]
   def change
-    add_reference :songs, :artist, null: true, foreign_key: true
-    add_reference :songs, :album, null: true, foreign_key: true
+    add_reference :songs, :artist, null: false, foreign_key: true
+    add_reference :songs, :album, null: false, foreign_key: true
   end
 end
