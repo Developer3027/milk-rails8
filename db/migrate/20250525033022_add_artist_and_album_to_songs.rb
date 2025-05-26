fa7aa975138b0d47f80fc6303ad0e32a529cf87d
Mason Roberts <mrobe18@gmail.com>
2025-05-25 22:30:36 -0400

    allow null values for id in songs

--- db/migrate/20250525033022_add_artist_and_album_to_songs.rb
+++ db/migrate/20250525033022_add_artist_and_album_to_songs.rb
@@ -1,35 +1,5 @@
-class AddArtistAndAlbumToSongs < ActiveRecord::Migration[8.0]
-  def up
-    # Drop the old table (destructive! Only do this if you don't need the data.)
-    drop_table :songs
-
-    # Recreate the table with artist and album references
-    create_table :songs do |t|
-      t.string :artist
-      t.string :album
-      t.string :title
-      t.string :song_image_url
-      t.string :song_file_url
-      t.references :artist, null: true, foreign_key: true
-      t.references :album, null: true, foreign_key: true
-      # Add other columns your app needs (duration, track_number, etc.)
-      t.timestamps
-    end
-  end
-
-  def down
-    # Rollback: Drop the new table and recreate the old structure (if needed)
-    drop_table :songs
-
-    # Example of old structure (adjust based on your original schema)
-    create_table :songs do |t|
-      t.string :title
-      # ... other columns that existed before ...
-      t.timestamps
-    end
-  end
-  def change
-    add_reference :songs, :artist, null: false, foreign_key: true
-    add_reference :songs, :album, null: false, foreign_key: true
-  end
+# db/migrate/XXXXXX_add_artist_and_album_to_songs.rb
+def change
+  add_reference :songs, :artist, foreign_key: true, null: true  # Allow null temporarily
+  add_reference :songs, :album, foreign_key: true, null: true  # Allow null temporarily
 end
