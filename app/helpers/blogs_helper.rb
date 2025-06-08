cc0afe97e09d56ea40af4134bf7cf5ac83b6c10b
Mason Roberts <mrobe18@gmail.com>
2025-06-08 15:45:46 -0400

    fixed code and layout issues

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,7 +1,7 @@
 module BlogsHelper
   def display_blog_body(blog)
     sanitize(blog.processed_body,
-             tags: %w[h1 h2 h3 h4 p a ul ol li strong em br img span div],
-             attributes: %w[href src id class style title alt])
+              tags: %w[h1 h2 h3 h4 p a ul ol li strong em br img span pre code div],
+              attributes: %w[href src id class style title alt])
   end
 end
